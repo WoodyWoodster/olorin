a1bf0a2edee0a843d2361eec95076e62f3ebd6d2
Andrew Woodward <arwoodward553@gmail.com>
2025-10-20 17:24:34 -0600

    chore: cleanup

--- elessar/app/controllers/users/sessions_controller.rb
+++ elessar/app/controllers/users/sessions_controller.rb
@@ -2,13 +2,8 @@
 
 class Users::SessionsController < Devise::SessionsController
   respond_to :json
-  before_action :configure_sign_in_params, only: [ :create ]
 
   private
-
-  def configure_sign_in_params
-    devise_parameter_sanitizer.permit(:sign_in, keys: [ :email, :password ])
-  end
 
   def respond_with(resource, _opts = {})
     render json: {
@@ -21,20 +16,8 @@ class Users::SessionsController < Devise::SessionsController
   end
 
   def respond_to_on_destroy
-    if request.headers["Authorization"].present?
-      jwt_payload = JWT.decode(request.headers["Authorization"].split(" ").last,
-                               ENV["DEVISE_JWT_SECRET_KEY"] || Rails.application.credentials.secret_key_base).first
-      current_user = User.find(jwt_payload["sub"])
-    end
-
-    if current_user
-      render json: {
-        message: "Logged out successfully."
-      }, status: :ok
-    else
-      render json: {
-        message: "Couldn't find an active session."
-      }, status: :unauthorized
-    end
+    render json: {
+      message: "Logged out successfully."
+    }, status: :ok
   end
 end
